6607d4bdec1df37d40bee2980f907459cb0aacb7
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2023-07-30 23:09:23 +0200

    fix

--- app.ts
+++ app.ts
@@ -13,6 +13,11 @@ connectDB()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello!')
